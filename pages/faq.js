e8ea89e43558dee055bc47a607783d026e1019b1
luancortez <luancortezdev@gmail.com>
2023-01-25 20:19:14 -0300

    first commit

---
+++ pages/faq.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import Head from 'next/head'
+
+import Link from '../src/components/Link';
+
+export async function getStaticProps() {
+
+    const faq = await axios.get('https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json')
+            .then(res => {
+                return res.data
+            })
+
+    return {
+        // will be passed to the page component as props
+        props: {
+            faq
+        }
+    }
+  }
+  
+
+  
+
+export default function FAQPage({faq, qualquercoisa}) {
+    // const [faq, setFaq] = useState([])
+    // useEffect(() => {
+        
+    // }, []) 
+    return (
+        <div>
+            <Head>
+                <title>FAQ - Alura Cases Campanha</title>
+            </Head>
+            <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
+            <Link href="/">
+                Ir para a home
+                
+            </Link>
+            <ul>
+				{/* {faq.map((faq) => ( */}
+                {faq.map(({question, answer}) => (
+					<li key={question}>
+						<article>
+							<h2>{question}</h2>
+							<p>{answer}</p>
+						</article>
+					</li>
+				))}
+            </ul>
+        </div>
+    )
+}
